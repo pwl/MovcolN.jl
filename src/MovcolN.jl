956fdd49f06f788e8b83ed087c6e773c9638c153
Paweł Biernat <pawel.biernat+github@gmail.com>
2014-11-21 12:19:14 +0100

    First working version of computeres

--- src/MovcolN.jl
+++ src/MovcolN.jl
@@ -7,9 +7,10 @@ export getcomputeres
 include("polynomials.jl")       # standard polynomial related
                                 # functions
 
-# create matrices A and B such that A*Fi = B*Gi/Hi, assuming that Fi
-# is evaluated at Gauss points and Gi is evaluated at Lobatto
-# points.  The dimensions of A and B are n×n and n×n+1 respectively.
+# create matrix AB=inv(A)*B with A and B such that A*Fi = B*Gi/Hi
+# (i.e. approximating F=∂ₓG), assuming that Fi is evaluated at Gauss
+# points and Gi is evaluated at Lobatto points.  The dimensions of A
+# and B are n×n and n×n+1 respectively.
 function generateAB(n; T=Float64)
     sg, _ = gauss(n)
     sl, _ = lobatto(n+1)
@@ -94,52 +95,117 @@ end
 #
 # generates the residual function of the equation eqn
 
-function getcomputeres(F::Function,G::Function)
+function getcomputeres(F,G,Bl,Br,M,Bxl,Bxr,tau,gamma,
+                       ns,      # number of derivatives to hold
+                       nu,      # number of dependent variables
+                       nx)      # number of mesh points
+
+    if !(iseven(ns))
+        error("ns should be even")
+    end
 
     T = Float64
 
-    ns = 2                      # number of collocation points has to
-                                # be even
-    nu = 1                      # number of dependent variables
-    nx = 2                      # number of mesh points
-    kmax = ns+1                 # maximal darivative is of rank kmax-1
+    kmax = ns+1                 # maximal darivative is of rank kmax-1 = ns
+    ns2 = Int(ns/2)
     sGauss, _ = gauss(ns)       # Gauss points
     sLobat, _ = lobatto(ns+1)   # Lobatto points
 
     QsGauss = map(x->generateQatx(ns,x,kmax=kmax),sGauss)
     QsLobat = map(x->generateQatx(ns,x,kmax=kmax),sLobat)
 
-    FatGauss = Array(T,nu,ns,  nx)
-    GatLobat = Array(T,nu,ns+1,nx)
+    FatGauss = Array(T,nu,ns  )
+    GatLobat = Array(T,nu,ns+1)
+
+    resu     = Array(T,nu,ns,  nx)
+    resx     = Array(T,        nx)
+    Mhalf    = Array(T,        nx-1)
+    Yhalf    = Array(T,        nx+1)
+
+    AB = generateAB(ns)
 
     function computeres{T}(t :: T,
                            x :: Vector{T}, xt :: Vector{T},
                            u :: Array{T,3},ut :: Array{T,3})
 
+        #######################################################################
+        # pde residua
+        #######################################################################
         for i = 1:nx-1
             h  =  x[i+1]- x[i]
             ht = xt[i+1]-xt[i]
+            ul = u[:,:,i  ]
+            ur = u[:,:,i+1]
+            utl= ut[:,:,i  ]
+            utr= ut[:,:,i+1]
 
             for j = 1:ns
-                ux  =  computeux(QsGauss[j],h,u[:,:,i],u[:,:,i+1])
-                uxt = computeutx(QsGauss[j],h,ht,xt[i],ut[:,:,i],ut[:,:,i+1],ux,u[:,:,i],u[:,:,i+1])
+                ux  =  computeux(QsGauss[j],h,ul,ur)
+                uxt = computeutx(QsGauss[j],h,ht,xt[i],utl,utr,ux,ul,ur)
                 xj  = x[i]+sGauss[j]*h
-                FatGauss[:,j,i] = F(t,xj,ux,uxt)
+                FatGauss[:,j] = F(t,xj,ux,uxt)
             end
 
             for j = 1:ns+1
-                ux  =  computeux(QsLobat[j],h,u[:,:,i],u[:,:,i+1])
-                uxt = computeutx(QsLobat[j],h,ht,xt[i],ut[:,:,i],ut[:,:,i+1],ux,u[:,:,i],u[:,:,i+1])
+                ux  =  computeux(QsLobat[j],h,ul,ur)
+                uxt = computeutx(QsLobat[j],h,ht,xt[i],utl,utr,ux,ul,ur)
                 xj  = x[i]+sLobat[j]*h
-                GatLobat[:,j,i] = G(t,xj,ux,uxt)
+                GatLobat[:,j] = G(t,xj,ux,uxt)
             end
 
-            @show A*F
-            @show B*G/h
+            resu[:,:,i] = FatGauss'-AB*GatLobat'/h
 
         end
 
+        # residua for the boundary conditions, Bl and Br should each
+        # return the vector of size at least nu*ns/2, the remaining
+        # boundary conditions are discarded
+        resBl = Bl(t,x[ 1],xt[ 1],u[:,:, 1],ut[:,:, 1])[1:nu*ns2]
+        resBr = Br(t,x[nx],xt[nx],u[:,:,nx],ut[:,:,nx])[1:nu*ns2]
+        resu[:,    1:ns2,nx] = reshape(resBl,nu,ns2)
+        resu[:,ns2+1:ns, nx] = reshape(resBr,nu,ns2)
+
+        #######################################################################
+        # mesh movement residua
+        #######################################################################
+        resx[ 1] = Bxl(t,x[ 1],xt[ 1],u[:,:, 1],ut[:,:, 1])
+        resx[nx] = Bxr(t,x[nx],xt[nx],u[:,:,nx],ut[:,:,nx])
+        for i = 1:nx-1
+            h  =  x[i+1]- x[i]
+            ht = xt[i+1]-xt[i]
+            ul = u[:,:,i  ]
+            ur = u[:,:,i+1]
+            utl= ut[:,:,i  ]
+            utr= ut[:,:,i+1]
+
+            # monitor function
+            if i==1 || i==nx-1
+                Mhalf[i] = (M(t,x[i],ul,utl)+M(t,x[i+1],ur,utr))/2
+            else
+                xhalf  = x[i]+h/2
+                xthalf = xt[i]+ht/2
+                uhalf  = computeux(QsLobat[ns2+1],h,ul,ur)
+                uthalf = computeutx(QsLobat[ns2+1],h,ht,xthalf,utl,utr,uhalf,ul,ur)
+                Mhalf[i] = M(t,xhalf,uhalf,uthalf)
+            end
+
+            Yhalf[i+1] = -tau*(xt[i+1]-xt[i])/(x[i+1]-x[i])^2+1/(x[i+1]-x[i])
+        end
+
+        # boundary conditions for Yhalf
+        Yhalf[ 1 ] = Yhalf[  2  ]
+        Yhalf[end] = Yhalf[end-1]
+
+        # compute the residua for mesh movement
+        for i = 2:nx-1
+            g=gamma*(gamma+1)
+            resx[i] = (Yhalf[i+1]-g*(Yhalf[i+2]-2*Yhalf[i+1]+Yhalf[i]))/Mhalf[i]-(Yhalf[i]-g*(Yhalf[i+1]-2*Yhalf[i]+Yhalf[i-1]))/Mhalf[i-1]
+        end
+
+        return resu, resx
+
     end
+
 end
 
 end # module
